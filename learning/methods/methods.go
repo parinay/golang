0b6a993aff4d839cdd847fe6be91b17e82183b94
parinay <parinay@gmail.com>
2019-07-11 13:55:58 +0530

    Go methods value and pointer receivers, methods with same names
    Method examples with
    - Value recivers
    - pointer receivers
    - methods with same names for different type(struct)

--- learning/methods/methods.go
+++ learning/methods/methods.go
@@ -1,17 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 type rect struct {
 	width, height int
 }
+type Circle struct {
+	r float64
+}
 
+// pointer receivers
+// method with same name area() for struct rect
 func (r *rect) area() int {
 	return r.width * r.height
 }
 
+// value receivers
 func (r rect) perim() int {
 	return 2*r.width + 2*r.height
+}
+
+// value receivers
+// method with same name area() for struct Circle
+
+func (c Circle) area() float64 {
+	return math.Pi * c.r * c.r
+}
+
+func (c Circle) perm() float64 {
+	return 2 * math.Pi * c.r
 }
 func main() {
 	r := rect{width: 10, height: 5}
@@ -22,4 +42,10 @@ func main() {
 	rp := &r
 	fmt.Println("area: ", rp.area())
 	fmt.Println("perim: ", rp.perim())
+
+	c := Circle{
+		r: 12,
+	}
+	fmt.Printf("Area of circle is %f", c.area())
+	fmt.Printf("Area of circle is %f", c.perm())
 }
