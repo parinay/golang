46fdda7d73f6a210a5000cc77997ee491ec3460f
parinay <parinay@gmail.com>
2019-07-12 09:04:27 +0530

    Go methods: pointer receivers vs value receivers
    - pointer receivers can be used when changes made to the receivers
    inside the method should be visible to the caller
    
    - pointer receivers can also be used where its expensive to copy
    a data structre between caller and callee

--- learning/methods1/main.go
+++ learning/methods1/main.go
@@ -4,8 +4,19 @@ import (
 	"fmt"
 )
 
+// value receivers
 func (e Employee) displaySalary() {
-	fmt.Printf("Salary of %s is %s%d", e.name, e.currency, e.salary)
+	fmt.Printf("Salary of %s is %s%d\n", e.name, e.currency, e.salary)
+}
+
+// value recivers - chnages are not reflected back
+func (e Employee) changeName(newName string) {
+	e.name = newName
+}
+
+// pointer reciver - changes are reflected back
+func (e *Employee) changeSalary(newSalary int) {
+	e.salary = newSalary
 }
 
 type Employee struct {
@@ -20,5 +31,11 @@ func main() {
 		salary:   50000,
 		currency: "USD ",
 	}
+	fmt.Println("Employee 1 - ", emp1)
 	emp1.displaySalary()
+	emp1.changeName("Mama")
+	fmt.Println("Employee 1 (changeName)- ", emp1)
+	emp1.changeSalary(0)
+	fmt.Println("Employee 1 (changeSalary)- ", emp1)
+
 }
