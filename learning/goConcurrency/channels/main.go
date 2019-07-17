b277a6294867344a0d3699f7f242e46f01abfc07
parinay <parinay@gmail.com>
2019-07-17 10:03:42 +0530

    GoConcurrency: channels, range over channels to receive
    - bi-directional channels
    - for over range to receive from channels
    - defer close(channel)
    - uni-directional channel

--- learning/goConcurrency/channels/main.go
+++ learning/goConcurrency/channels/main.go
@@ -8,12 +8,37 @@ func hello(done chan bool) {
 	done <- true
 }
 func main() {
+	fmt.Println("main function start")
 	// channel of type boolean
 	done := make(chan bool)
 	go hello(done)
 
-	// channel receiving data but done not store OR use the value
+	// channel receiving data but do not store OR use the value
 	<-done
 
-	fmt.Println("main function")
+	fmt.Println("main function - after channel receive")
+
+	number := 589
+
+	sqrch := make(chan int)
+	cubech := make(chan int)
+
+	go calcSquares(number, sqrch)
+	go calcCubes(number, cubech)
+
+	squares, cubes := <-sqrch, <-cubech
+
+	fmt.Printf("The number is 589\nsquares:%d \ncubes:%d \nFinal Output(squares+cubes):%d, \n", squares, cubes, squares+cubes)
+
+	// uni directional channel
+
+	sendch := make(chan<- int)
+	go sendData(sendch)
+
+	// fmt.Printlsn(<-sendch)
+
+	// chanl is bi-rectional in main()
+	chanl := make(chan int)
+	go sendData(chanl)
+	fmt.Println(<-chanl)
 }
