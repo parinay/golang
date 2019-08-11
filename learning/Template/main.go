806c8de29b4fc697e53359e5eed6d4c6045acff7
parinay <parinay@gmail.com>
2019-08-11 23:23:15 +0530

    Templates in go, examples

---
+++ learning/Template/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"log"
+	"os"
+	"text/template"
+)
+
+func main() {
+	tpl, err := template.ParseFiles("index.gohtml")
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	type User struct {
+		Name   string
+		Coupon string
+		Amount int64
+	}
+
+	user := User{
+		Name:   "Rick",
+		Coupon: "TERANUMBNERLAGGAYA",
+		Amount: 5000,
+	}
+
+	err = tpl.Execute(os.Stdout, user)
+
+	if err != nil {
+		log.Println("executing template:", err)
+	}
+}
