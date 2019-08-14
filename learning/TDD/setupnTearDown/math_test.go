51b7f6e17acb3af26901b5f92c45eefda1e175b2
parinay <parinay@gmail.com>
2019-08-15 00:09:16 +0530

    TDD Setup and tearDown functions example with Make file
    https://blog.karenuorteva.fi/go-unit-test-setup-and-teardown-db1601a796f2#.2aherx2z5

--- learning/TDD/setupnTearDown/math_test.go
+++ learning/TDD/setupnTearDown/math_test.go
@@ -2,6 +2,19 @@ package math
 
 import "testing"
 
+func setupTestCase(t *testing.T) func(t *testing.T) {
+	t.Log("setup test case")
+	return func(t *testing.T) {
+		t.Log("teardown test case")
+	}
+}
+
+func setupSubTest(t *testing.T) func(t *testing.T) {
+	t.Log("setup subtest")
+	return func(t *testing.T) {
+		t.Log("teardown sub test")
+	}
+}
 func TestAddition(t *testing.T) {
 	cases := []struct {
 		name     string
@@ -14,8 +27,13 @@ func TestAddition(t *testing.T) {
 		{"zero", 0, 0, 0},
 	}
 
+	tearDownTestCase := setupTestCase(t)
+	defer tearDownTestCase(t)
 	for _, tc := range cases {
 		t.Run(tc.name, func(t *testing.T) {
+			teardownSubTest := setupSubTest(t)
+			defer teardownSubTest(t)
+
 			result := Sum(tc.a, tc.b)
 			if result != tc.expected {
 				t.Fatalf("expected sum %v, but got %v", tc.expected, result)
